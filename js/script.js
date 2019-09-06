8fb7ed3f17b430ef06abc0541be578129ba4eb5d
Rado Jedro <radoslaw.jedro@gmail.com>
2019-09-06 22:15:38 +0100

    Add function generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -42,8 +42,51 @@ const titleClickHandler = function(event){
     console.log(targetArticle);
   }
   
-  const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
+
+  const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log(titleList);
+  titleList.innerHTML = '';
+
+  /* find all the articles and save them to variable: articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(titleList);
+
+  let html = '';
+
+  for(let article of articles){
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+
+    /* find the title element */
+    /* ... */
+
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into html variable */
+    html = html + linkHTML;
   }
+
+  titleList.innerHTML = html;
+  
+}
+generateTitleLinks();
+
+const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
