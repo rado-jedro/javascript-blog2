307bdf9b0b39bf1ce21984f88c2dddd2b6106dd0
Rado Jedro <radoslaw.jedro@gmail.com>
2019-09-06 21:03:26 +0100

    Add remove class 'active'

--- js/script.js
+++ js/script.js
@@ -6,23 +6,30 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
 */
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
+    console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
+  
     const activeLinks = document.querySelectorAll('.titles a.active');
-
+  
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
   
-    /* remove class 'active' from all articles */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+  
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles  = document.querySelectorAll('.post');
-
+    
     for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-  }
-  
+        activeArticle.classList.remove('active');
+      }
+
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
