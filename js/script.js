ab2b544ec612c17b6874fb774e578d91e66ee6f4
Rado Jedro <radoslaw.jedro@gmail.com>
2019-09-06 21:10:16 +0100

    Add function to display article after click

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const titleClickHandler = function(event){
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-  
+    event.preventDefault();
     /* [DONE] remove class 'active' from all article links  */
   
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -25,16 +25,21 @@ const titleClickHandler = function(event){
   
     /* [DONE] remove class 'active' from all articles */
     const activeArticles  = document.querySelectorAll('.post');
-    
+
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
       }
 
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    console.log(targetArticle);
   }
   
   const links = document.querySelectorAll('.titles a');
